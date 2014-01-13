d6f97c634367647987652d4caffb154cfecb12ac
vietdung <vietdung175@yahoo.com>
2014-01-13 17:23:02 +0700

    add more branch

--- HowToUse/src/Test001.java
+++ HowToUse/src/Test001.java
@@ -8,6 +8,6 @@
  */
 public class Test001 {
 	public static void main(String[] args) {
-		System.out.println("Last christmas, i give you my heart. the very next day, you told me you're gay");
+		System.out.println("VietDung's branch");
 	}
 }
